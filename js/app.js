85781d121a474f3af6fa86fb2b3b89122db18458
devLuis-Duarte <luis.saldanha@aluno.ifsp.edu.br>
2023-10-09 15:10:25 -0300

    Pegando elementos por id e add novos valores

--- js/app.js
+++ js/app.js
@@ -3,12 +3,42 @@ console.log(
     
     document.getElementsByClassName("carro")[1].innerHTML,
     
-    document.getElementsByTagName("div")[4].innerHTML,
+    document.getElementsByTagName("div")[4].innerHTML, 
     
     document.getElementsByTagName("p")[0].innerHTML,
+    
+    document.querySelector("#p1").innerHTML,
 
-    document.querySelector("#p2")
+    document.getElementById("futebol-europeu").querySelectorAll(".times")[1].innerHTML
+
+                                            
+     //getElementById() retorna o elemento pelo id passado como parametro
+
+    //getElementsByClassName() retorna o(s) elementos pelo nome da classe
+                                            //especificada no parametro
+                                            
+    //getElementsByTagName() retorna o(s) elemento(s) pela tag passada
+                                            //como parametro
+                                             
+    //querySelector() retorna o elemento que ele encontrar primeiro 
+    // a nao ser que seja especificado um id como parametro
+                                            
+    //querySelectorAll() retorna um objeto nodelist representando
+    // todos os elementos do documento que estejam de acordo com o 
+    //parametro informado
+                                             
 )
 alert(screen.width)
+
+var fruta = document.getElementById("fruta")
+fruta.innerHTML = "Manga"
+fruta.style.color = "orange"
+fruta.style.backgroundColor = "#333"
+fruta.style.height = "400px"
+
+var imagem = document.getElementById("imagem")
+imagem.src = "html.jpg"
+imagem.width = "300"
+imagem.height = "300"
 
 document.getElementById("largura").innerHTML = screen.width
